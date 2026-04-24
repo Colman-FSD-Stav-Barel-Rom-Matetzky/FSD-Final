35c1953d4c14a53ce4992bdb06a6d73644ea697d
Stav Barel <stavbarel2003@gmail.com>
2026-04-24 18:09:22 +0300

    server https

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,24 @@
 import 'dotenv/config';
 import { app, connectDB } from './app';
 import { AppConfig } from './config/app.config';
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
 
 const PORT = AppConfig.port;
 
 void connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('development');
+    http.createServer(app).listen(PORT);
+  }
+
+  const options = {
+    key: fs.readFileSync('./client-key.pem'),
+    cert: fs.readFileSync('./client-cert.pem'),
+  };
+
+  https.createServer(options, app).listen(process.env.HTTPS_PORT);
 });
 
 export default app;
